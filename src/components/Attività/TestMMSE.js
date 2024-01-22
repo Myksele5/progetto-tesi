ba2456144327248c465c6f35b2c28b243c8feab8
Myksele5 <mikseleee@gmail.com>
2024-01-22 20:49:00 +0100

    Continua il lavoro su test MMSE

--- src/components/Attività/TestMMSE.js
+++ src/components/Attività/TestMMSE.js
@@ -1,15 +1,25 @@
+import styles from "./TestMMSE.module.css";
 import { useState } from "react";
 import GenericButton from "../UI/GenericButton";
+import GenericAlternativeButton from "../UI/GenericAlternativeButton";
 
 function TestMMSE(){
     const [testIniziato, setTestIniziato] = useState(false);
+    const [sezioneCognitiva, setSezioneCognitiva] = useState(1);
 
     function iniziaTest(){
         setTestIniziato(true);
     }
 
+    function prossimaSezioneCognitiva(){
+        setSezioneCognitiva((prevSezione) => (prevSezione + 1));
+    }
+    function precedenteSezioneCognitiva(){
+        setSezioneCognitiva((prevSezione) => (prevSezione - 1));
+    }
+
     return(
-        <div>
+        <div className={styles.test_container}>
             {!testIniziato && 
             <>
                 <label>Ciao sono il test<br/></label>
@@ -23,7 +33,7 @@ function TestMMSE(){
             </>
             }
 
-            {testIniziato &&
+            {testIniziato && sezioneCognitiva === 1 &&
             <>
                 <h1>AREA COGNITIVA N.1</h1>
                 <h2>Come ti chiami?</h2>
@@ -34,6 +44,7 @@ function TestMMSE(){
                 <h2>Che giorno è oggi?</h2>
                 <label>GIORNO</label>
                 <select>
+                    <option hidden>--</option>
                     <option>1</option>
                     <option>2</option>
                     <option>3</option>
@@ -42,6 +53,7 @@ function TestMMSE(){
                 </select>
                 <label>MESE</label>
                 <select>
+                    <option hidden>--</option>
                     <option>01</option>
                     <option>02</option>
                     <option>03</option>
@@ -57,6 +69,7 @@ function TestMMSE(){
                 </select>
                 <label>ANNO</label>
                 <select>
+                    <option hidden>--</option>
                     <option>2022</option>
                     <option>2023</option>
                     <option>2024</option>
@@ -65,6 +78,56 @@ function TestMMSE(){
                 </select>
                 <h2>In quale città ci troviamo?</h2>
                 <input placeholder="inserisci luogo.."></input>
+                
+            </>
+            }
+
+            {testIniziato && sezioneCognitiva === 2 &&
+            <>
+                <h1>AREA COGNITIVA N.2</h1>
+                <h2>In questo esericizio ti verranno mostrate tre parole che dovrai ricordare.</h2>
+                <h2>Avrai a disposizione 15 secondi per leggerle e memorizzarle, dopo di che scrivi le parole che ricordi come risposte</h2>
+
+                <h5>PAROLA 1</h5>
+                <h5>PAROLA 2</h5>
+                <h5>PAROLA 3</h5>
+            </>
+            }
+
+            {testIniziato && sezioneCognitiva === 3 &&
+            <>
+                <h1>AREA COGNITIVA N.3</h1>
+                <h1 style={{color: "red"}}>!!!!! In alternativa qui si possono mettere esercizi matematici semplici !!!!!</h1>
+                <h2>Adesso ti verrà mostrata una parola ed il tuo obiettivo è scriverla al contrario</h2>
+
+                <h5>PAROLAALCONTRARIO</h5>
+
+                <label>Riscrivi la parola al contrario</label>
+                <input></input>
+            </>
+            }
+            {testIniziato && sezioneCognitiva === 4 &&
+            <>
+                <h1>AREA COGNITIVA N.4</h1>
+                <h2>Mostra due immagini di oggetti al posto di questa frase</h2>
+                <label>Oggetto 1</label>
+                <input></input>
+                <label>Oggetto 2</label>
+                <input></input>
+            </>
+            }
+            {testIniziato &&
+            <>
+                <GenericAlternativeButton
+                    onClick={prossimaSezioneCognitiva}
+                    buttonText={"Prossima domanda"}
+                >
+                </GenericAlternativeButton>
+                <GenericAlternativeButton
+                    onClick={precedenteSezioneCognitiva}
+                    buttonText={"Domanda precedente"}
+                >
+                </GenericAlternativeButton>
             </>
             }
         </div>
